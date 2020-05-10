dbc8394f8273e7f4dc8f46ad5b30cb925cfb51ab
utkarsh4321 <utkarsh4321@users.noreply.github.com>
2020-05-10 19:59:57 +0000

    deploy: 24c2846ad8fe0ea22587c271c48654f5f808d632

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5faffffc7bac11c7240b253f8d845159.js"
+  "/Booklist/precache-manifest.ffc8545acf4004fdb9df2769767dd8f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Booklist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
